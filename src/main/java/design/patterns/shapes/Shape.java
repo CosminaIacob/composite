5d2769112f34a0d9967bfde42b5f04e557d7301b
Cosmina Iacob <Cosmina-Gabriela.IACOB@externals.adidas.com>
2024-03-13 18:33:31 +0200

    Initial commit

---
+++ src/main/java/design/patterns/shapes/Shape.java
@@ -0,0 +1,25 @@
+package design.patterns.shapes;
+
+import java.awt.*;
+
+public interface Shape {
+    int getX();
+
+    int getY();
+
+    int getWidth();
+
+    int getHeight();
+
+    void move(int x, int y);
+
+    boolean isInsideBounds(int x, int y);
+
+    void select();
+
+    void unSelect();
+
+    boolean isSelected();
+
+    void paint(Graphics graphics);
+}
